4b1503c78012ee660d438c4b90a467aaca58fc85
Gül Çavdar <cavdargul@gmail.com>
2026-07-13 12:47:06 +0300

    Initial Commit

---
+++ SplitBill/SplitBillApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitBillApp.swift
+//  SplitBill
+//
+//  Created by Gül Çavdar on 13.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct SplitBillApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
